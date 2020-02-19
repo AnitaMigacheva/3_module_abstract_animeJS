e755131348c860e82ef5b7a9113d5e8134473e23
AnitaMigacheva <55283508+AnitaMigacheva@users.noreply.github.com>
2020-02-19 20:58:09 +0300

    -

---
+++ main.js
@@ -0,0 +1,46 @@
+let propsAnim = anime({
+    targets: '.red.red1',
+    translateX: 290,
+    rotate: [12, 12],
+    direction: 'alternate',
+    loop: true,
+    autoplay: false,
+    easing: 'spring(1, 80, 10, 0)'
+  })
+
+let rect = document.getElementById('redRect1')
+rect.onclick = propsAnim.play
+
+let path = anime.path('path')
+let svgMotion = anime({
+  targets: '.black.black1',
+  translateX: path('x'),
+  translateY: path('y'),
+  rotate: path('angle'),
+  easing: 'linear',
+  duration: 15000,
+  autoplay: false,
+  loop: true
+})
+
+let blackRect1 = document.getElementById('blackRect1')
+blackRect1.onclick = svgMotion.play
+
+let keyframes = anime({
+  targets: '.white.white2',
+  rotate: [12, 12],
+  keyframes: [
+    {translateY: -40},
+    {translateX: 250},
+    {translateY: 40},
+    {translateX: 0},
+    {translateY: 0}
+  ],
+  duration: 4000,
+  easing: 'easeOutElastic(1, .8)',
+  loop: true,
+  autoplay: false
+});
+
+let whiteRect2 = document.getElementById('whiteRect2')
+whiteRect2.onclick = keyframes.play
